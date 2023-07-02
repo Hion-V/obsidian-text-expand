4e6ce1168bb65d5dd9e5b0479bfb99af26355c8b
Andreas <the.hion.v@gmail.com>
2023-07-02 03:31:16 +0200

    Revert "Changed main.ts"
    This reverts commit 841e1e8eb5a9799a4f8856ea580334c70e114221.

--- src/main.ts
+++ src/main.ts
@@ -136,14 +136,12 @@ export default class TextExpander extends Plugin {
 
     async onload() {
         this.addSettingTab(new SettingTab(this.app, this));
-        const indexOfRendered = ctx.containerEl.classList.value.indexOf("markdown-rendered");
-        if(indexOfRendered < 0){
-            this.registerMarkdownCodeBlockProcessor('expander', (source, el, ctx) => {
-                el
-                    .createDiv()
-                    .createEl('button', {text: 'Run expand query'})
-                    .addEventListener('click', this.init.bind(this, false, ctx.getSectionInfo(el).lineStart))
-            }
+
+        this.registerMarkdownCodeBlockProcessor('expander', (source, el, ctx) => {
+            el
+                .createDiv()
+                .createEl('button', {text: 'Run expand query'})
+                .addEventListener('click', this.init.bind(this, false, ctx.getSectionInfo(el).lineStart))
         });
 
         this.addCommand({
